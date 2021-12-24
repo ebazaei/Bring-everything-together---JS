cb42f8d74e85129b1d438a659af485b2a3a4950c
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-24 10:52:58 +0100

    after gallery photo

---
+++ script/Archive.js
@@ -0,0 +1,78 @@
+//Form Validator
+
+// const myForm = document.querySelector('#my-form');
+// const nameInput = document.querySelector('#name');
+// const emailInput = document.querySelector('#email');
+// const msg = document.querySelector('.msg');
+
+// myForm.addEventListener('submit', onsubmit);
+
+// function onsubmit(e){
+//     e.preventDefault();
+//     console.log(nameInput.value);
+
+//     if(nameInput.value === ''){
+//         msg.classList.add('error');
+//         msg.innerHTML = 'please insert Your Name';
+
+//         setTimeout(() => msg.remove(), 5000)
+
+//     }else if(emailInput.value === ''){
+//         msg.classList.add('error');
+//         msg.innerHTML = 'please insert Your Email';
+
+//         setTimeout(() => msg.remove(), 5000)
+
+//     }else {
+//         msg.classList.add('true');
+//         msg.innerHTML = 'Success';
+
+//     }
+// }
+
+
+
+//HTML CSS
+
+const minDv = document.getElementById('demo2')
+
+function imgcreator(testnm) {
+    var imgsrc = "./img/picture"
+
+    for (let i = 0; i < testnm+1; i++) {
+        //var x = document.createElement("IMG");
+        //x.setAttribute("src", imgsrc + i + ".jpg");
+        // document.body.appendChild(x);
+        // minDv.innerHTML += '<img src="https://www.w3schools.com/jsref/img_pulpit.jpg" alt="">'
+
+        var imgSrcTag = `<img src="./img/picture${i}.jpg" alt="">`;
+        minDv.innerHTML += imgSrcTag
+    }  
+    return x;
+}
+
+
+imgcreator(3)
+
+// minDv.innerHTML += '<img src="https://www.w3schools.com/jsref/img_pulpit.jpg" alt=""> <br><br><br>'
+// minDv.innerHTML += '<img src="https://www.w3schools.com/jsref/img_pulpit.jpg" alt=""> <br>'
+
+
+// document.getElementById("demo").innerHTML = imgcreator(5);
+
+
+// function imgcreator(testnm) {
+//     var imgsrc = "./img/picture"
+
+//     for (let i = 0; i < testnm+1; i++) {
+//         var x = document.createElement("IMG");
+//         x.setAttribute("src", imgsrc + i + ".jpg");
+//         // document.body.appendChild(x);
+//         // minDv.innerHTML += '<img src="https://www.w3schools.com/jsref/img_pulpit.jpg" alt="">'
+//     }  
+//     return x;
+// }
+
+
+
+
--- script/script.js
+++ script/script.js
@@ -27,12 +27,28 @@ function onsubmit(e){
         msg.classList.add('true');
         msg.innerHTML = 'Success';
 
-
-        //clear fields
-        nameInput.value = "";
-        emailInput.value = ""
     }
 }
+
+
+
+//HTML CSS
+
+const minDv = document.getElementById('photogallery')
+
+function imgcreator(testnm) {
+    var imgsrc = "./img/picture"
+
+    for (let i = 1; i < testnm+1; i++) {
+        var imgSrcTag = `<div class="col-sm m-2"><a href=""> <img src="./img/picture${i}.jpg" alt="" class="img-fluid"> </a></div>`;
+        minDv.innerHTML += imgSrcTag
+    }  
+    return testnm;
+}
+
+imgcreator(5)
+
+
 
 
 
